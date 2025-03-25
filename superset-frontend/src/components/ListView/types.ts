afaaaa89612682be46d0f3f7a07d15037ef1e1dc
evelinaREnil <evelina.nilsson@rystadenergy.com>
2025-03-25 15:39:17 +0100

    add allwaysVisible to filter type

--- superset-frontend/src/components/ListView/types.ts
+++ superset-frontend/src/components/ListView/types.ts
@@ -63,6 +63,7 @@ export interface Filter {
   paginate?: boolean;
   filterType?: 'toggle' | 'icon'  ;
   iconType?: 'status' | 'certified' | 'favorite' | 'type';
+  allwaysVisible?: boolean;
 }
 
 export type Filters = Filter[];
